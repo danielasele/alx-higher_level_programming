3acd5f0d4c95c3c58aaeaa797d7296d355857ba0
danielasele <daniase2113@gmail.com>
2024-01-03 16:48:22 -0800

    6-print_comb3.py task updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,4 +2,5 @@
 
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}".format(num1 * 10 + num2), end=(", " if (num1, num2) != (8, 9) else "\n"))
+        print("{:02d}".format(num1 * 10 + num2), end=(", " if (num1, num2) != (8, 9)
+                                                       else "\n"))
