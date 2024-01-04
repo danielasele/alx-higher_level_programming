d2e388eb2c74f5fea1ca3c6d1a0e04198bb5200c
danielasele <daniase2113@gmail.com>
2024-01-03 16:45:01 -0800

    6-print_comb3.py task updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for num in range(100):
-    print("{:02d}".format(num), end=(", " if num < 99 else "\n"))
+for num1 in range(10):
+    for num2 in range(num1 + 1, 10):
+        print("{:02d}".format(num1 * 10 + num2), end=(", " if (num1, num2) != (8, 9) else "\n"))
