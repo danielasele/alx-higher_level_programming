96fb7994980f21ab98cbca88797d2ca7ba7c947f
danielasele <daniase2113@gmail.com>
2024-01-03 17:08:21 -0800

    7-islower.py task updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,12 +1,2 @@
-#!/usr/bin/env python3
-
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
-
-if __name__ == "__main__":
-    # Sample test cases
-    print(islower("a"))  # True
-    print(islower("H"))  # False
-    print(islower("A"))  # False
-    print(islower("3"))  # False
-    print(islower("g"))  # True
