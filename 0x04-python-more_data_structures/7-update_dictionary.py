641e25823a7a64253a136fc94012fe8608c5c329
danielasele <daniase2113@gmail.com>
2024-03-10 08:12:51 -0700

    7-update_dictionary.py updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,14 @@
 def update_dictionary(a_dictionary, key, value):
+    """
+    Replace or add key/value in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to update.
+        key (str): The key to replace or add.
+        value (any): The value corresponding to the key.
+
+    Returns:
+        dict: The updated dictionary.
+    """
     a_dictionary[key] = value
     return a_dictionary
