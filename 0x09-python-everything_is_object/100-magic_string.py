b7b81cbb705dffaa81f130c6f01295a73d42a387
danielasele <daniase2113@gmail.com>
2024-05-29 09:56:13 -0700

    Task 29 completed

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
-def magic_string(magic_list=[]):
-
-    magic_list.append("BestSchool")
-    return ", ".join(magic_list)
+#!/usr/bin/python3
+def magic_string():
+    magic_string.count = getattr (magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in  range (magic_string.count)])
